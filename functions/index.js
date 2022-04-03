d248ec3811f609c6b6064854abba4a8f1d1e692f
Matgsan <matgsan@me.com>
2022-04-03 14:19:08 -0400

    alerts

--- functions/index.js
+++ functions/index.js
@@ -16,7 +16,7 @@ exports.onAlertCreate = functions.firestore
     const promises = [];
     db.collection("notification_tokens").get().then(snapshot => {
       snapshot.forEach(doc => {
-        promises.add(sendAlert(doc.data().token, alert));
+        promises.push(sendAlert(doc.data().token, alert));
       });
     }).catch(err => {
       console.log(err);
@@ -24,7 +24,46 @@ exports.onAlertCreate = functions.firestore
     return Promise.all(promises);
   });
 
+  exports.apiFetch = functions.pubsub.schedule('every 12 hours').onRun(() => {
+    return getAlerts();
+  });
 
+
+  async function getAlerts(){
+   const response = await axios.get('https://apiprevmet3.inmet.gov.br/avisos/ativos');
+   if(response){
+    const batch = db.batch();
+      const {data} = response;
+      const {hoje, futuro} = data;
+      const alertsRef = db.collection('alerts');
+      const alertsFutureRef = db.collection('alerts_future');
+      hoje.forEach(item => {
+        if(item.municipios.includes('Petrópolis')){
+          batch.set(alertsRef.doc(item.id),{
+            id: item.id,
+            descricao: item.descricao,
+            aviso_cor: item.aviso_cor,
+            severidade: item.severidade,
+            riscos: item.riscos,
+            instrucoes: item.instrucoes,
+          });
+        }
+      });
+      futuro.forEach(item => {
+        if(item.municipios.includes('Petrópolis')){
+          batch.set(alertsFutureRef.doc(item.id),{
+            id: item.id,
+            descricao: item.descricao,
+            aviso_cor: item.aviso_cor,
+            severidade: item.severidade,
+            riscos: item.riscos,
+            instrucoes: item.instrucoes,
+          });
+        }
+      });
+      await batch.commit();
+   }
+  }
  async function sendAlert(token, alert){
    console.log('sendAlert');
   return axios.post('https://exp.host/--/api/v2/push/send', 
